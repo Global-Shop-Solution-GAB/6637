9f202ecf5778fc3f5b45cdee3503644ffe4648c2
Service Web <sw@gssmail.com>
2024-03-19 11:01:06 -0500

    Commit changes in C:\Websites\Git\emarlow\6637\GCG_6637_APRVRS.sql

---
+++ GCG_6637_APRVRS.sql
@@ -0,0 +1,10 @@
+-- Database: GLOBALPLA Metadata version: 2 Exported: Feb 24, 2022
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6637_APRVRS" USING 'GCG_6637_APRVRS.mkd' PAGESIZE=4096 (
+ "GS_USER" VARCHAR(8) NOT NULL,
+ "APPROVER" VARCHAR(8) NOT NULL );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
